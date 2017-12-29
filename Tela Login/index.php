82bd9d32d26aef353118ebdb1b217787d1d9c6e1
Leo <lnddru@gmail.com>
2017-12-29 00:02:02 -0200

    Arquivos selecionados

---
+++ Tela Login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tela login</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  
+  <div>
+    
+  	<div class="tela">
+
+  		<form action="" method="">
+  
+         <label for="Name">Nome:</label>
+  		   <input type="email" name="email" placeholder="email:">
+
+  		   <label for="Name">Password:</label>
+  		   <input type="Password" name="email" placeholder="Password:">
+
+  		   <button type="button" value="enviar">Enviar</button>
+
+  		   <img src="img/c.jpg" width="180px" height="100px">
+  			
+  		</form>
+  	</div>
+    </div>
+</body>
+</html>
